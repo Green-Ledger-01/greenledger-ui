d001d3d157dc68432a2956c0de6def8fd1f052c5
oumaoumag <ouma.godwin10@gmail.com>
2025-06-11 16:03:21 +0300

    Add Navbar component to AppRoutes for improved navigation

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/NavBar';
 import Login from '../pages/Login';
 import Dashboard from '../pages/Dashboard';
 import Tokenization from '../pages/Tokenization';
@@ -9,13 +10,16 @@ import SupplyChainExplorer from '../pages/SupplyChainExplorer';
 const AppRoutes = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tokenization" element={<Tokenization />} />
-        <Route path="/transfer-ownership" element={<TransferOwnership />} />
-        <Route path="/supply-chain-explorer" element={<SupplyChainExplorer />} />
-      </Routes>
+      <Navbar />
+      <div className="container mx-auto p-4 pt-20">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tokenization" element={<Tokenization />} />
+          <Route path="/transfer-ownership" element={<TransferOwnership />} />
+          <Route path="/supply-chain-explorer" element={<SupplyChainExplorer />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
