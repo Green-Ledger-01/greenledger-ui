99cefdcce245495f00269fc5abed0c78a89a522f
oumaoumag <ouma.godwin10@gmail.com>
2025-06-11 18:26:46 +0300

    Add custom global styles and remove BrowserRouter from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,6 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { config } from './config/wagmiConfig';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Remove BrowserRouter from here as it will be in AppRoutes
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
