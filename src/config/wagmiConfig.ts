618f9ee6ef74acb3f61ec1f5d4f5850ec42ef8d1
joseph30222 <ochejoseph16@gmail.com>
2025-06-14 08:14:09 +0000

    Fix @coinbase/wallet-sdk import issue and include wagmiConfig.ts and script

--- src/config/wagmiConfig.ts
+++ src/config/wagmiConfig.ts
@@ -8,7 +8,7 @@ export const chains = [liskSepolia] as const
 export const config = createConfig({
   connectors: [
     injected(),
-    coinbaseWallet({ appName: 'GreenLedger' }),
+    
     walletConnect({ projectId: '48eb3c0dc304ad679c805fba58632a31' }),
   ],
   chains,
