1964259cf256ef3c13ae2127a509b1976520aaa3
oumouma oumouma <ouma.godwin10@gmail.com>
2025-06-19 05:26:44 +0300

    src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import AppRoutes from './routes/AppRoutes'
+import React from 'react';
+import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-green-100 via-white to-green-200">
-      <AppRoutes />
-    </div>
-  );
+  return <AppRoutes />;
 }
