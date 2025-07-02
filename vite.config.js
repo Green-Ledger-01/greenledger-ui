6f2898889a940c1585b5208703efc5ef602f9bd8
oumaoumag <ouma.godwin10@gmail.com>
2025-07-02 13:33:32 +0300

    feat: change JSX runtime from automatic to classic in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: 'automatic',
+      jsxRuntime: 'classic',
       jsxImportSource: 'react',
       babel: {
         plugins: []
